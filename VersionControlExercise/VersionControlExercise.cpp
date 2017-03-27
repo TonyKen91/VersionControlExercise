cd37298f66ed210622342fe7d7a0a611c858fd58
TK <antoine.kenneth.odi@gmail.com>
2017-03-27 11:14:15 +1030

    added things in the first line

--- VersionControlExercise/VersionControlExercise.cpp
+++ VersionControlExercise/VersionControlExercise.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 int main()
 {
-	cout << "Add something" << endl;
+	cout << "Add something somewhere somehow by someone" << endl;
 	cout << "Add something else" << endl;
 	return 0;
 }
