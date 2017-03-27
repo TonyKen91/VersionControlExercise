9a58d555238615fed425d6f328943fd46efd1e53
MD <markodrca@live.com>
2017-03-27 12:13:41 +1030

    ..

--- VersionControlExercise/VersionControlExercise.cpp
+++ VersionControlExercise/VersionControlExercise.cpp
@@ -18,14 +18,7 @@ int main()
 	cout << "Random line" << endl;
 	cout << "Editing random branch" << endl;
 	cout << "Interesting" << endl;
-<<<<<<< HEAD
-=======
 	cout << "Tree making" << endl;
-<<<<<<< HEAD
->>>>>>> origin/Lets_make_a_tree
-=======
-=======
->>>>>>> master
 	cout << "Branching a branch" << endl;
 	cout << "Don't ruin my tree" << endl;
 	cout << "Add branch else" << endl;
@@ -33,29 +26,9 @@ int main()
 	cout << "Add another branch else" << endl;
 	cout << "goodbye World!" << endl;
 	cout << "Yay more branches" << endl;
-<<<<<<< HEAD
->>>>>>> More_branches
-=======
-		cout << "other" << endl;
->>>>>>> Head_Branch
-<<<<<<< HEAD
->>>>>>> origin/Branch_Of_Branch
-=======
->>>>>>> Branch_Of_Branch
->>>>>>> New_Branch
-<<<<<<< HEAD
->>>>>>> origin/Lets_make_a_tree
-=======
->>>>>>> Lets_make_a_tree
-<<<<<<< HEAD
->>>>>>> origin/Branch_of_SUp_World
-=======
->>>>>>> Branch_of_SUp_World
->>>>>>> origin/Call_it_something
-=======
+	cout << "other" << endl;
 	cout << "other" << endl;
 	cout << "HELP" << endl;
->>>>>>> master
 	return 0;
 }
 
