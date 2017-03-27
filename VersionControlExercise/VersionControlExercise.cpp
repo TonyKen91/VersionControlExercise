25d851c105c1c48a3e1510c30174c39f454b321b
MD <markodrca@live.com>
2017-03-27 12:06:02 +1030

    Finalish

--- VersionControlExercise/VersionControlExercise.cpp
+++ VersionControlExercise/VersionControlExercise.cpp
@@ -13,51 +13,20 @@ int main()
 	cout << "Another line to merge" << endl;
 	cout << "Add random" << endl;
 	cout << "Add something else" << endl;
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> Lets_make_a_tree
 	cout << "Add branch" << endl;
 	cout << "Add another branch" << endl;
 	cout << "Random line" << endl;
 	cout << "Editing random branch" << endl;
-<<<<<<< HEAD
 	cout << "Interesting" << endl;
 	cout << "Tree making" << endl;
-=======
 	cout << "Branching a branch" << endl;
-<<<<<<< HEAD
 	cout << "Don't ruin my tree" << endl;
-=======
-<<<<<<< HEAD
-=======
-	>> >> >> > origin / Call_it_something
->>>>>>> Head_Branch
-
-<<<<<<< HEAD
 	cout << "Add branch else" << endl;
 	cout << "Add another part" << endl;
-=======
-
-
-<<<<<<< HEAD
 	cout << "Add another branch else" << endl;
-
-
-
 	cout << "goodbye World!" << endl;
-=======
 	cout << "Yay more branches" << endl;
->>>>>>> More_branches
-=======
-		cout << "other" << endl;
->>>>>>> Head_Branch
->>>>>>> Branch_Of_Branch
->>>>>>> New_Branch
->>>>>>> Lets_make_a_tree
->>>>>>> Branch_of_SUp_World
+	cout << "other" << endl;
 	return 0;
 }
 
