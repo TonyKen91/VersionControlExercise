1102ace8653794110f2cd513bc4c978b2a16b037
MD <markodrca@live.com>
2017-03-27 11:27:36 +1030

    Added new line

--- VersionControlExercise/VersionControlExercise.cpp
+++ VersionControlExercise/VersionControlExercise.cpp
@@ -8,12 +8,10 @@ using namespace std;
 
 int main()
 {
-<<<<<<< .merge_file_a07052
 	cout << "Add something somewhere somehow by someone" << endl;
-=======
 	cout << "Sup World!" << endl;
->>>>>>> .merge_file_a07384
 	cout << "Add something else" << endl;
+	cout << "New Line added" << endl;
 	return 0;
 }
 
